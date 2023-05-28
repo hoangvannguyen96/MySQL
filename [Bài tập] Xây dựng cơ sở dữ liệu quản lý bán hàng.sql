f119d34b0b09dcb3f9a7a32eb8be002ca08573dd
hoangvannguyen96 <hoangvannguyen23011996@gmail.com>
2023-05-28 11:01:48 +0700

    [Bài tập] Xây dựng cơ sở dữ liệu quản lý bán hàng

--- [Bài tập] Xây dựng cơ sở dữ liệu quản lý bán hàng.sql
+++ [Bài tập] Xây dựng cơ sở dữ liệu quản lý bán hàng.sql
@@ -1,1 +1,28 @@
-SELECT * FROM store.customer;
+-- create database store;
+use store;
+create table Customer(
+idcutomer int not null primary key,
+name varchar(255),
+age int 
+);
+create table Oder(
+idoder int not null primary key,
+idcutomer int not null,
+date date,
+totalprice float,
+foreign key (idcutomer) references Customer(idcutomer)
+);
+create table product(
+idproduct int not null primary key,
+name varchar(255),
+price float
+);
+create table oderitems(
+idoder int,
+idproduct int,
+quantity int,
+foreign key (idoder) references oder(idoder),
+foreign key (idproduct) references product(idproduct)
+);
+alter table oderitems
+add primary key(idoder,idproduct)
