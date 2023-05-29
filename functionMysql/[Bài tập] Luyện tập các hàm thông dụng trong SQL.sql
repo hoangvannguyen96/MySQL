0a0a6cc3239de1ff0ca42b67522f9617081d9613
hoangvannguyen96 <hoangvannguyen23011996@gmail.com>
2023-05-29 10:11:13 +0700

    [Bài tập] Luyện tập các hàm thông dụng trong SQL

---
+++ functionMysql/[Bài tập] Luyện tập các hàm thông dụng trong SQL.sql
@@ -0,0 +1,15 @@
+use quanlysinhvien;
+
+select subname, max(credit)
+from subject
+group by subname
+having max(credit)= all (select max(credit) from subject);
+
+select * from quanlysinhvien.subject
+where credit = (select max(credit) from subject);
+
+select s.*,avg(mark) as avgmark 
+from student s
+join mark m on m.studentid=s.studentid
+group by s.studentid,s.studentname,s.address,s.phone,s.status,s.classid
+order by classid
